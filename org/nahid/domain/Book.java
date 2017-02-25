469a5100c746da124ffc28ceb328611c97120d7e
nahid <nahidvaf@yahoo.com>
2017-02-25 15:37:56 +0100

    wrong spelling of directory.

---
+++ org/nahid/domain/Book.java
@@ -0,0 +1,10 @@
+package org.nahid.domain;
+
+import java.math.BigDecimal;
+
+public class Book {
+    private String title;
+    private String author;
+    private BigDecimal price;
+
+}
---
+++ org/nahid/domain/BookList.java
@@ -0,0 +1,7 @@
+package org.nahid.domain;
+
+public interface BookList {
+    public Book[] list(String searchString);
+    public boolean add(Book book, int quantity);
+    public int[] buy(Book... books);
+}
